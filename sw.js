4fd3e40b4c039340061ee72ef39f58b6f007437b
gaborider09-cell <gaborider09@gmail.com>
2026-07-23 11:40:59 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'convertix-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve el recurso en caché si existe, si no, lo busca en la red.
+        return response || fetch(event.request);
+      })
+  );
+});
